417a60629c2efdc5080826a36267d66f28b0eff9
q8minecraft <q812121212@hotmail.com>
2015-08-17 22:25:39 +0300

    commit comment

--- src/Reporter.java
+++ src/Reporter.java
@@ -3,6 +3,7 @@ import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.Image;
 import java.awt.SystemColor;
+import java.awt.TextArea;
 import java.awt.Toolkit;
 import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.StringSelection;
@@ -24,12 +25,17 @@ public class Reporter {
 
 	private JFrame frame;
 	private JTextField textFieldName;
-	private JTextField textFieldOffense;
-	private JTextField textFieldProof;
-	private JComboBox<String> comboBoxRank;
 	private JComboBox<String> comboBoxPerm;
 	private JTextArea textAreaResult;
-
+	private TextArea textAreaProof;
+	private TextArea textAreaOffense;
+	private JComboBox<String> comboBoxColor;
+	private JLabel lblName;
+	private JLabel lblProof;
+	private JLabel lblOffense;
+	private JLabel lblDoYouGive;
+	private JComboBox<String> comboBoxRank;
+	private JLabel lblRank;
 	/**
 	 * Launch the application.
 	 */
@@ -62,7 +68,7 @@ public class Reporter {
 	private void initialize() {
 		frame = new JFrame();
 		frame.getContentPane().setForeground(SystemColor.desktop);
-		frame.setBounds(100, 100, 812, 493);
+		frame.setBounds(100, 100, 812, 482);
 		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		java.net.URL url = this.getClass().getResource("/logo2.png");
@@ -76,17 +82,16 @@ public class Reporter {
 		btnNewButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				String name, offense, proof, comborank, comboperm;
+				String name, offense, proof,comboperm, comborank;
 				name = textFieldName.getText();
-				offense = textFieldOffense.getText();
-				proof = textFieldProof.getText();
-				comborank = comboBoxRank.getSelectedItem().toString();
+				offense = textAreaOffense.getText();
+				proof = textAreaProof.getText();
 				comboperm = comboBoxPerm.getSelectedItem().toString();
-				System.out.println(name + offense + proof + comborank
-						+ comboperm);
-				textAreaResult.setText("[" + comborank + "] " + name + "\r\n"
+				comborank = comboBoxRank.getSelectedItem().toString();
+				System.out.println(name + offense + proof + comboperm);
+				textAreaResult.setText("[" + comborank + "]" + name + "\r\n"
 						+ "Offense: " + offense + "\r\n" + "Proof: " + proof
-						+ "\r\n" + "Permission to share: " + comboperm);
+						+ "\r\n" + "Permission to share: " + comboperm + "\r\n" + "\r\n" + "Skin: http://www.minecraft-skin-viewer.net/?username=" + name);
 				String myString = textAreaResult.getText();
 				StringSelection stringSelection = new StringSelection(myString);
 				Clipboard clpbrd = Toolkit.getDefaultToolkit()
@@ -95,9 +100,15 @@ public class Reporter {
 				JOptionPane.showMessageDialog(null,
 						"Report copied to clipboard!" + "\r\n"
 								+ "Use CTRL + V for easy paste ;)");
+				Reporter.this.textFieldName.setText("");
+		        Reporter.this.textAreaOffense.setText("");
+		        Reporter.this.textAreaProof.setText("");
+		        Reporter.this.comboBoxRank.setSelectedItem("Default");
+		        Reporter.this.comboBoxPerm.setSelectedItem("Yes.");
+		        Reporter.this.textAreaResult.setText("");
 			}
 		});
-		btnNewButton.setBounds(10, 386, 322, 58);
+		btnNewButton.setBounds(10, 374, 322, 58);
 		frame.getContentPane().add(btnNewButton);
 
 		textFieldName = new JTextField();
@@ -105,67 +116,55 @@ public class Reporter {
 		textFieldName.setBounds(10, 50, 322, 34);
 		frame.getContentPane().add(textFieldName);
 		textFieldName.setColumns(10);
+		
+		
+		
+		textAreaOffense = new TextArea();
+		textAreaOffense.setBounds(10, 256, 322, 42);
+		frame.getContentPane().add(textAreaOffense);
+		
+		textAreaProof = new TextArea();
+		textAreaProof.setBounds(10, 188, 322, 42);
+		frame.getContentPane().add(textAreaProof);
 
-		textFieldOffense = new JTextField();
-		textFieldOffense.setToolTipText("Offense");
-		textFieldOffense.setBounds(10, 188, 322, 34);
-		frame.getContentPane().add(textFieldOffense);
-		textFieldOffense.setColumns(10);
-
-		textFieldProof = new JTextField();
-		textFieldProof.setToolTipText("Proof");
-		textFieldProof.setBounds(10, 258, 322, 34);
-		frame.getContentPane().add(textFieldProof);
-		textFieldProof.setColumns(10);
-
-		JLabel lblName = new JLabel("Name");
+		lblName = new JLabel("Name");
 		lblName.setFont(new Font("Tahoma", Font.BOLD, 12));
 		lblName.setForeground(SystemColor.desktop);
 		lblName.setBounds(147, 25, 46, 14);
 		frame.getContentPane().add(lblName);
 
-		JLabel lblNewLabel = new JLabel("Rank");
-		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 12));
-		lblNewLabel.setForeground(SystemColor.desktop);
-		lblNewLabel.setBounds(147, 95, 46, 14);
-		frame.getContentPane().add(lblNewLabel);
+		lblProof = new JLabel("Proof");
+		lblProof.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblProof.setForeground(SystemColor.desktop);
+		lblProof.setBounds(147, 161, 39, 14);
+		frame.getContentPane().add(lblProof);
 
-		JLabel lblNewLabel_1 = new JLabel("");
-		lblNewLabel_1.setBounds(147, 208, 46, 14);
-		frame.getContentPane().add(lblNewLabel_1);
 
-		JLabel lblNewLabel_2 = new JLabel("Proof");
-		lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD, 12));
-		lblNewLabel_2.setForeground(SystemColor.desktop);
-		lblNewLabel_2.setBounds(147, 233, 39, 14);
-		frame.getContentPane().add(lblNewLabel_2);
+		lblOffense = new JLabel("Offense");
+		lblOffense.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblOffense.setForeground(SystemColor.desktop);
+		lblOffense.setBounds(147, 236, 58, 14);
+		frame.getContentPane().add(lblOffense);
 
-		comboBoxRank = new JComboBox<>();
-		comboBoxRank.setModel(new DefaultComboBoxModel<>(new String[] { "Default", "VIP",
-				"VIP+", "MVP", "MVP+" }));
-		comboBoxRank.setToolTipText("Rank");
-		comboBoxRank.setBounds(10, 120, 322, 34);
-		frame.getContentPane().add(comboBoxRank);
-
-		JLabel lblNewLabel_3 = new JLabel("Offense");
-		lblNewLabel_3.setFont(new Font("Tahoma", Font.BOLD, 12));
-		lblNewLabel_3.setForeground(SystemColor.desktop);
-		lblNewLabel_3.setBounds(147, 165, 58, 14);
-		frame.getContentPane().add(lblNewLabel_3);
-
-		JLabel lblDoYouGive = new JLabel(
+		lblDoYouGive = new JLabel(
 				"Do you give the staff permission to share your proof?");
 		lblDoYouGive.setFont(new Font("Tahoma", Font.BOLD, 12));
-		lblDoYouGive.setForeground(SystemColor.desktop);
+		lblDoYouGive.setForeground(new Color(0, 0, 0));
 		lblDoYouGive.setBackground(Color.WHITE);
-		lblDoYouGive.setBounds(10, 300, 345, 14);
+		lblDoYouGive.setBounds(10, 304, 345, 14);
 		frame.getContentPane().add(lblDoYouGive);
 
+		lblRank = new JLabel("Rank");
+		lblRank.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblRank.setForeground(SystemColor.desktop);
+		lblRank.setBounds(147, 95, 46, 14);
+		frame.getContentPane().add(lblRank);
+		
 		comboBoxPerm = new JComboBox<>();
 		comboBoxPerm.setModel(new DefaultComboBoxModel<>(new String[] { "Yes.",
 				"No." }));
 		comboBoxPerm.setToolTipText("");
-		comboBoxPerm.setBounds(10, 318, 322, 34);
+		comboBoxPerm.setBounds(10, 329, 322, 34);
 		frame.getContentPane().add(comboBoxPerm);
 
 		textAreaResult = new JTextArea();
@@ -175,30 +174,76 @@ public class Reporter {
 		textAreaResult.setBounds(504, 90, 282, 269);
 		textAreaResult.setColumns(10);
 		frame.getContentPane().add(textAreaResult);
-
-		JButton btnNewButton_2 = new JButton("Clear");
-		btnNewButton_2.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				JOptionPane.showMessageDialog(null,
-						"This button does nothing \r\n at the moment!");
-			}
-		});
-		btnNewButton_2.setFont(new Font("Yu Mincho", Font.BOLD, 15));
-		btnNewButton_2.setBounds(335, 386, 159, 58);
-		frame.getContentPane().add(btnNewButton_2);
-
+		
+		
+		comboBoxRank = new JComboBox<>();
+		comboBoxRank.setModel(new DefaultComboBoxModel<>(new String[] {
+				"Default", "VIP", "VIP+", "MVP", "MVP+" }));
+		comboBoxRank.setToolTipText("Rank");
+		comboBoxRank.setBounds(10, 120, 322, 34);
+		frame.getContentPane().add(comboBoxRank);
+		
 		JLabel label = new JLabel("");
+		label.setBackground(SystemColor.menu);
 		label.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		label.setBounds(0, 0, 796, 470);
+		label.setBounds(0, 0, 796, 443);
 		frame.getContentPane().add(label);
 		Image img = new ImageIcon(this.getClass().getResource("/logo2.png"))
 				.getImage();
 		label.setIcon(new ImageIcon(img));
 		
-		JLabel lblMadeByProfessorzed = new JLabel("Made by Professor_Zed");
-		lblMadeByProfessorzed.setFont(new Font("AR BERKLEY", Font.PLAIN, 15));
-		lblMadeByProfessorzed.setBounds(627, 429, 159, 14);
-		frame.getContentPane().add(lblMadeByProfessorzed);
+		comboBoxColor = new JComboBox();
+		comboBoxColor.setModel(new DefaultComboBoxModel(new String[] {"Original (Black)", "Red", "Blue", "Green"}));
+		comboBoxColor.setBounds(565, 10, 122, 20);
+		frame.getContentPane().add(comboBoxColor);
+		
+		
+		JLabel lblColors = new JLabel("Colors:");
+		lblColors.setBounds(523, 13, 46, 14);
+		frame.getContentPane().add(lblColors);
+		
+		
+		
+		JButton btnConfirm = new JButton("Confirm");
+		btnConfirm.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(comboBoxColor.getSelectedItem().toString() == "Original (Black)") {
+					lblName.setForeground(Color.BLACK);
+					lblProof.setForeground(Color.BLACK);
+					lblOffense.setForeground(Color.BLACK);
+					lblDoYouGive.setForeground(Color.BLACK);
+					lblRank.setForeground(Color.BLACK);
+					JOptionPane.showMessageDialog(null, "Changed colors to the original " + "\r\n" + "Color (Black)");
+				}
+				if(comboBoxColor.getSelectedItem().toString() == "Red") {
+					lblName.setForeground(Color.RED);
+					lblProof.setForeground(Color.RED);
+					lblOffense.setForeground(Color.RED);
+					lblDoYouGive.setForeground(Color.RED);
+					lblRank.setForeground(Color.RED);
+					JOptionPane.showMessageDialog(null, "Changed colors to Red.");
+				}
+				if(comboBoxColor.getSelectedItem().toString() == "Blue") {
+					lblName.setForeground(Color.BLUE);
+					lblProof.setForeground(Color.BLUE);
+					lblOffense.setForeground(Color.BLUE);
+					lblDoYouGive.setForeground(Color.BLUE);
+					lblRank.setForeground(Color.BLUE);
+					JOptionPane.showMessageDialog(null, "Changed colors to Blue.");
+				}
+				if(comboBoxColor.getSelectedItem().toString() == "Green") {
+					lblName.setForeground(Color.GREEN);
+					lblProof.setForeground(Color.GREEN);
+					lblOffense.setForeground(Color.GREEN);
+					lblDoYouGive.setForeground(Color.GREEN);
+					lblRank.setForeground(Color.GREEN);
+					
+					JOptionPane.showMessageDialog(null, "Changed colors to Green.");
+				}
+			}
+		});
+		btnConfirm.setBounds(697, 9, 89, 23);
+		frame.getContentPane().add(btnConfirm);
 		
 	}
 }
